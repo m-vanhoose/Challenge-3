f3d6f2fced720df3a55f972abe66e76cf1271695
m-vanhoose <m-vanhoose@live.com>
2023-02-12 12:27:30 -0500

    Finished generator function

--- script.js
+++ script.js
@@ -1,32 +1,61 @@
 // Assignment code here
-// Math.floor(Math.random() * 10)
+
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
 function generatePassword() {
-  var charLength = prompt("How long do you want your password to be?")
+  
+  let charLength = prompt("How long do you want your password to be?")
+  if (charLength > 128 || charLength < 8) {
+    return alert("INVALID. LENGTH NEEDS TO BE BETWEEN 8 AND 128.")
+  } 
 
-  if(charLength > 128 || charLength < 8) {
-    return "NOT A VALID LENGTH"
+  let charUpper = confirm("Do you want uppercase letters?")
+  if (charUpper) { 
+    charUpper= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+     alert ("UPPERCASE SELECTED")
+  }else {
+     alert ("UPPERCASE NOT SELECTED")
   }
-
-  var string = "qwertyuiop123456789"
-  var newPassword = ""
+  let charLower = confirm("Do you want lowercase letters?")
+  if (charLower) {
+    charLower = 'abcdefghijklmnopqrstuvwxyz'
+      alert ("LOWERCASE SELECTED")
+  }else {
+      alert ("LOWERCASE NOT SELECTED")
+  }
+  let charNumber = confirm("Do you want numbers?")
+  if (charNumber) {
+    charNumber = '0123456789'
+    alert ('NUMBERS SELECTED')
+  }else {
+    alert ('NUMBERS NOT SELECTED')
+  }
+  
+  let charSpecial = confirm("Do you want special characters?")
+  if (charSpecial) {
+    charSpecial = '`~!@#$%^&*(()-=_+[]{}|;:,./<>?'
+    alert ('SPECIAL CHARACTERS SELECTED')
+  }else {
+    alert ('SPECIAL CHARACTERS NOT SELECTED')
+  }
+  let string = charUpper + charLower + charNumber + charSpecial
+  let newPassword = ""
 
   for(i=0; i< charLength; i++) {
-    var randomIndex = Math.floor(Math.random() * string.length)
+    let randomIndex = Math.floor(Math.random() * string.length)
     newPassword += string[randomIndex]
-    console.log(newPassword)
+    // console.log(newPassword)
   }
 
   return newPassword
